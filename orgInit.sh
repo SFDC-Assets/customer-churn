04764b4f774662510671a812407bc5a57dd2c530
esandoval30 <esandoval@salesforce.com>
2020-07-28 14:41:09 +0100

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 10 -s -w 3
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 3
 sfdx force:source:push
 sfdx force:user:password:generate
 sfdx shane:data:dates:update -r 7-14-2020
