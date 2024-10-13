c8290eae104b65dae9832709ed6debde923e4001
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-12 23:45:56 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,10 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix customer -o churn.workshop
-sfdx force:source:push
-sfdx shane:user:password:set -g User -l User -p salesforce1
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p customer -e churn.workshop
+sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
 sfdx shane:data:dates:update -r 7-14-2020
-sfdx force:data:bulk:upsert -s Contact -f data-modified/velostar-contacts.csv -i External_Id__c
-sfdx force:data:bulk:upsert -s Contact -f data-modified/velostar-new-customers.csv -i External_Id__c
+sf data bulk upsert -s Contact -f data-modified/velostar-contacts.csv -i External_Id__c
+sf data bulk upsert -s Contact -f data-modified/velostar-new-customers.csv -i External_Id__c
 #Install EPB Model Accuracy Package
 sfdx force:package:install -p 04t4J000002ASSJ
 #Open org
-sfdx force:org:open -p /lightning/setup/SetupOneHome/home
-#sfdx force:org:open -p /lightning/page/home
+sf org open
