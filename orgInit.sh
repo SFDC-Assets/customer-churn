5925ec2b5a7d14f6b0909642211d66d12a528ef3
zhou059 <zhou059@gmail.com>
2024-10-28 22:35:20 -0700

    remove .sf

--- orgInit.sh
+++ orgInit.sh
@@ -2,8 +2,7 @@ sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p cus
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
 sfdx shane:data:dates:update -r 7-14-2020
-sf data bulk upsert -s Contact -f data-modified/velostar-contacts.csv -i External_Id__c
-sf data bulk upsert -s Contact -f data-modified/velostar-new-customers.csv -i External_Id__c
+sf automig load -d automig
 #Install EPB Model Accuracy Package
 sfdx force:package:install -p 04t4J000002ASSJ
 #Open org
