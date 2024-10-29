e4acfb98e9e56be3462a845c21fbe6125753d618
zhou059 <zhou059@gmail.com>
2024-10-28 22:48:46 -0700

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p customer -e churn.workshop
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
