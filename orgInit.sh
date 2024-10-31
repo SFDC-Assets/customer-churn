8ce19a2fa471604caaec70380f0756028229c784
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:42:52 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p customer -e churn.workshop
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
 sfdx shane:data:dates:update -r 7-14-2020
 sf automig load -d automig
-#Install EPB Model Accuracy Package
-sfdx force:package:install -p 04t4J000002ASSJ
-#Open org
+sf package install -p 04t4J000002ASSJ
 sf org open
